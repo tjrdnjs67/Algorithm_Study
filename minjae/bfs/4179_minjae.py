9542e2cd59d3e7bf21f5db20274b91b33b349833
minjae <limecats0331@gmail.com>
2022-07-28 20:59:25 +0900

    7/28 fail to solve

--- minjae/bfs/4179_minjae.py
+++ minjae/bfs/4179_minjae.py
@@ -1,75 +1,52 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-from collections import deque
-
-board = []
-
-def bfs(board,location,fire_location):
-    #상하좌우
-    dy = [-1,1,0,0]
-    dx = [0,0,-1,1]
-
-    que = deque()
-    que.append(location)
-    fire_que = deque()
-    for f in fire_locations:
-        fire_que.append(f)
-    visit = []
-    fired = []
-    count = 0
-    while que:
-        showBoard(board)
-        visit = que.popleft()
-        if fire_que:
-            fired = fire_que.popleft()
-        else:
-            fired = [-1,-1]
-        count += 1
-        for i in range(4):
-            y = visit[0] + dy[i]
-            x = visit[1] + dx[i]
-            fireY = fired[0] + dy[i]
-            fireX = fired[1] + dx[i]
-            if( 0 > x or x >= len(board[0]) or 0 > y or y >= len(board) ):
-                return count
-            if( 0 > fireX or fireX >= len(board[0]) or 0 > fireY or fireY >= len(board)):
-                continue
-            #불이랑 친구랑 한번에 연산해야됨
-            if board[y][x] == ".":
-                board[y][x] = "J"
-                if board[fireY][fireX] == "." or board[fireY][fireX] == "J":
-                    que.append([y,x])
-                    if board[fireY][fireX] == "J":
-                        board[fireY][fireX] = "F"
-                        que.remove([fireY,fireX])
-                    board[fireY][fireX] = "F"
-                    fire_que.append([fireY,fireX])
-            ##위에서 여기까지 수정해야됨
-    return -1
-
 def showBoard(board):
-    print("--------------")
     for line in board:
         print(line)
 
-if __name__ == "__main__":
-    N, M = map(int,input().split(" "))
+n, m = map(int, input().split())
+graph = []
 
-    for _ in range(N):
-        board.append(list(input().strip()))
+for i in range(n):
+    graph.append(list(input().rstrip()))
+    if 'J' in graph[i]:
+        q = deque([(0, i, graph[i].index('J'))])
 
-    start_location = []
-    fire_locations = []
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == "J":
-                start_location = [i,j]
-            if board[i][j] == "F":
-                fire_locations.append([i,j])
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 'F':
+            q.append((-1, i, j))
 
-    result = bfs(board,start_location,fire_locations)
-    if result == -1:
-        print("IMPOSSIBLE")
-    else:
-        print(result)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+ans = 'IMPOSSIBLE'
+
+while q:
+    #time으로 불인지 지훈인지 확인함
+    time, x, y = q.popleft()
+    showBoard(graph)
+
+    # 지훈이 탈출
+    if time > -1 and graph[x][y] != 'F' and (x == 0 or y == 0 or x == n - 1 or y == m - 1):
+        ans = time + 1
+        break
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        #범위랑 벽을 거름
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != '#':
+
+            # 지훈이 이동
+            if time > -1 and graph[nx][ny] == '.':
+                graph[nx][ny] = 'J'
+                q.append((time + 1, nx, ny))
+
+            # 불 퍼뜨리기
+            elif time == -1 and graph[nx][ny] != 'F':
+                graph[nx][ny] = 'F'
+                q.append((-1, nx, ny))
+
+print(ans)
