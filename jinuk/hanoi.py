770fcd6be07f8bd21df2a7efe858258cd3e893ee
정진욱 <dsa04156@naver.com>
2022-07-30 14:51:56 +0900

    Update hanoi.py

--- jinuk/hanoi.py
+++ jinuk/hanoi.py
@@ -1,10 +1,10 @@
-def hanoi(N,a,b):
+def hanoi(N,a,b,c):
     if N == 1:
         print(a,b)
         return
-    hanoi(N-1,a,6-a-b)
+    hanoi(N-1,a,c,b)
     print(a,b)
-    hanoi(N-1,6-a-b, b)
+    hanoi(N-1,c, b,a)
 N = int(input())
-print(2==N-1)
-hanoi(N,1,3)
+print(2**N-1)
+hanoi(N,1,3,2)
