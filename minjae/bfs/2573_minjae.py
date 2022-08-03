4ccdc668907521efe7e26c946562f11130bd9ca0
minjae <limecats0331@gmail.com>
2022-08-03 20:50:12 +0900

    2573 timeout

---
+++ minjae/bfs/2573_minjae.py
@@ -0,0 +1,74 @@
+import sys
+
+input = sys.stdin.readline
+from collections import deque
+
+board = []
+visited = []
+
+def bfs(board, start, visited):
+    #savedBoard = [line[:] for line in board]
+    zero = []
+    tmp = 0
+    # 상하좌우
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    que = deque([start])
+    while que:
+        tmp += 1
+        visit = que.popleft()
+        visited[visit[0]][visit[1]] = True
+        for i in range(4):
+            y = visit[0] + dy[i]
+            x = visit[1] + dx[i]
+            if 0 > x or x >= len(board[0]) or 0 > y or y >= len(board):
+                continue
+            # 연결 찾기
+            if board[y][x] > 0 and (not visited[y][x]):
+                que.append([y, x])
+                visited[y][x] = True
+            # 주변에 바다가 있고
+            elif board[y][x] == 0:
+                #완전히 녹지 않았으면
+                if board[visit[0]][visit[1]] > 1:
+                    board[visit[0]][visit[1]] -= 1
+                #다른 것에 영향이 생길 수 있으니깐 위치만 다른 곳에 저장해둠
+                elif board[visit[0]][visit[1]] == 1:
+                    zero.append(visit)
+    for lo in zero:
+        board[lo[0]][lo[1]] = 0
+    #print(tmp)
+    #showBoard()
+    return board
+
+
+def showBoard():
+    print("-----------")
+    for line in board:
+        print(line)
+
+
+if __name__ == "__main__":
+    row, col = map(int, input().split(" "))
+    board = []
+    for _ in range(row):
+        board.append(list(map(int, input().split(" "))))
+
+    result = -1
+    count = 0
+    while count < 2:
+        result += 1
+        visited = [[False for _ in range(col)] for _ in range(row)]
+        count = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] != 0 and not visited[i][j]:
+                    #print(result, [i,j])
+                    board = bfs(board, [i, j], visited)
+                    count += 1
+
+    print(result)
+
+## timeout
+## python3 34%
+## pypy3 64%
--- minjae/recursive/2447_minjae.py
+++ minjae/recursive/2447_minjae.py
@@ -3,20 +3,20 @@ input = sys.stdin.readline
 
 output = []
 
-def star(num):
-    if num == 1:
+def star(num,end):
+    if num == end*3:
         return
     compare = [ i + (num//3) for i in range(num//3)]
     for i in range(len(output)):
         for j in range(len(output)):
             if (i % num) in compare and (j % num) in compare:
                 output[i][j] = " "
-    star(num//3)
+    star(num*3,end)
 
 if __name__ == "__main__":
     N = int(input())
-    output = [["*" for _ in range(N)] for _ in range(N)]
-    star(N)
+    output = [["*"]*N]*N
+    star(3,N)
     for line in output:
-        print("".join(line))
-
+        sys.stdout.write("".join(line))
+        sys.stdout.write("\n")
